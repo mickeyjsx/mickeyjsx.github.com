47224fdb9ddd36632f87a008e417c362c5a7187d
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2017-09-03 14:17:30 +0800

    clean

--- .porscherc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  entry: { app: './src/index.jsx' },
-  extraBabelPlugins: [
-    ['mickey-model-loader', { loaderOptions: { regExp: /\.js$/ } }],
-    'transform-runtime',
-    'transform-decorators-legacy',
-    ['import', { 'libraryName': 'antd', 'style': true }]
-  ],
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# mickey-vstar
-A web app to show your or others GitHub repos stars. (Inspired by [vstar](https://github.com/sinchang/vstar))
